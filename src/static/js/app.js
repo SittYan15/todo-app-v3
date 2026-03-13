9901b88f7f5aa9054ee97154a09730bc21b5712f
SittYan15 <sittyanhtun@gmail.com>
2026-03-13 09:31:39 +0700

    abc3

--- src/static/js/app.js
+++ src/static/js/app.js
@@ -4,7 +4,7 @@ function App() {
         <Container>
             <Row>
                 <Col md={{ offset: 3, span: 6 }}>
-                    <h1>Sitt Yan Htun's To Do List with auto trigger!!</h1>
+                    <h1>Sitt Yan Htun's To Do List with auto trigger!!!</h1>
                     <TodoListCard />
                 </Col>
             </Row>
